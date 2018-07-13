cd427a9ac36194bc926bf20a0f73333c97ef9c9a
Bruno Oliveira <bruno@esss.com.br>
2018-07-13 10:24:38 -0300

    Drop pypy testing on Travis
    Unfortunately depending on pandas complicates testing with pypy

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
     ],
