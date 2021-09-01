67ceae789e9a20b0d05f525eb926c88f510b30d4
Erik <erihe251@student.liu.se>
2021-09-01 13:30:22 +0200

    Correct description of --force-regen

--- src/pytest_regressions/plugin.py
+++ src/pytest_regressions/plugin.py
@@ -29,9 +29,9 @@ def data_regression(datadir, original_datadir, request):
     data directory.
 
     Subsequent runs against the same data will now compare against the generated file and pass
-    if the dicts are equal, or fail showing a diff otherwise. To regenerate the data,
+    if the dicts are equal, or fail showing a diff otherwise. To regenerate data for failed tests,
     either set `force_regen` attribute to True or pass the `--force-regen` flag to pytest
-    which will regenerate the data files for all tests. Make sure to diff the files to ensure
+    which will regenerate the data files for all failed tests. Make sure to diff the files to ensure
     the new changes are expected and not regressions.
 
     The dict may be anything serializable by the `yaml` library.
