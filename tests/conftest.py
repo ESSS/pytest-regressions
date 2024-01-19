3880ae1cdab84dab1693fb8397bacb9f8a935542
Ron Arbo <ron.arbo@gmail.com>
2024-01-19 12:58:33 -0500

    remove new tests for documentation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,1 @@
 pytest_plugins = "pytester"
-
-
-def pytest_addoption(parser):
-    """Optional user specified data dir paths"""
-    parser.addini("datadir", "my custom datadir for pytest-regressions")
-    parser.addini(
-        "original_datadir", "my custom original_datadir for pytest-regressions"
-    )
