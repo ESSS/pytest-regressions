3a2ce4ec6474b6b3f7bba8d556fa1371fd23ea6d
Tsvika Shapira <tsvikas@gmail.com>
2026-01-08 15:51:21 +0200

    added python 3.14 for tests and metadata

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Programming Language :: Python :: Implementation :: CPython",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
